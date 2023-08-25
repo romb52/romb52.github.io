7ae32fc988ebad81f7a957d53df8af2460b8d357
Roman Chertilin <42869624+romb52@users.noreply.github.com>
2023-08-25 12:58:09 +0300

    add close by click bg
    ++

--- DZ_Events_2/js/script.js
+++ DZ_Events_2/js/script.js
@@ -1,15 +1,22 @@
-// Створіть HTML-сторінку з кнопкою Відкрити і модальним
+// 1. Створіть HTML-сторінку з кнопкою Відкрити і модальним
 // вікном. На модальному вікні має бути текст і кнопка Закрити.
 // Спочатку модальне вікно не відображається. При натисканні
 // на кнопку Відкрити з’являється модальне вікно, натомість на-
 // тискаючи кнопку Закрити – зникає.
+// 2. При кліке на бекграунд модальне окно зникає.
 
 const modalBlock = document.getElementById('openModalBlock');
 
-function openModal() {
-	modalBlock.classList.add('active');
+function openModal() { //відкривається модальне вікно
+  modalBlock.classList.add('active');
 }
 
-function closeModal() {
-	modalBlock.classList.remove('active');
+function closeModal() { // зачиняється модальне вікно
+  modalBlock.classList.remove('active');
+}
+
+function closeModalByClickBg(e) { // якщо клікнули на бекграунд (на body) модальне вікно зникає, якщо є
+  if (modalBlock.classList.contains('active') && e.target.nodeName == 'BODY') {
+    closeModal();
+  }
 }
