c9f85db5803e75d93fa9be3ab58911593153208b
romb52 <rom.bom1980@gmail.com>
2023-09-13 15:42:30 +0300

    Merge branch 'main' of https://github.com/romb52/romb52.github.io

--- DZ_Events_2/js/script.js
+++ DZ_Events_2/js/script.js
@@ -6,17 +6,24 @@
 // 2. При кліке на бекграунд модальне окно зникає.
 
 const modalBlock = document.getElementById('openModalBlock');
+const openModalBlockWraper = document.getElementById('openModalBlockWraper');
 
 function openModal() { //відкривається модальне вікно
   modalBlock.classList.add('active');
+  openModalBlockWraper.classList.add('active');
 }
 
 function closeModal() { // зачиняється модальне вікно
   modalBlock.classList.remove('active');
+  openModalBlockWraper.classList.remove('active');
 }
 
-function closeModalByClickBg(e) { // якщо клікнули на бекграунд (на body) модальне вікно зникає, якщо є
-  if (modalBlock.classList.contains('active') && e.target.nodeName == 'BODY') {
+
+
+openModalBlockWraper.onclick = closeModalByClickBg;
+
+function closeModalByClickBg(e) { //      якщо клікнули на бекграунд  модальне вікно зникає, якщо є
+   if (modalBlock.classList.contains('active') && e.target.id === 'openModalBlockWraper') {
     closeModal();
-  }
+   }
 }
