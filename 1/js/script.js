163e8cfdb2076ff489a2cdc087e51979103da5dd
romb52 <rom.bom1980@gmail.com>
2023-09-29 22:59:05 +0300

    +

--- 1/js/script.js
+++ 1/js/script.js
@@ -1,10 +1,13 @@
 const arrFrut = ['apple', 'pearch', 'orange', 'mango', 'lemon', 'cocumber', 'apple', 'pearch', 'orange', 'mango', 'lemon', 'cocumber', 'apple', 'pearch', 'orange', 'mango', 'lemon', 'cocumber'];
 const arrColors = ['red', 'blue', 'green', 'yellow', 'purple', 'orange'];
+
+
 arrFrut.forEach((item, i) => {
     const el = $('<div>');
     el.text(item);
     el.css('background-color', arrColors[Math.floor(Math.random() * arrColors.length)]);
     el.css('padding', '10px');
+    el.attr('data-color', el.css('background-color'))
     $('#wrap').append(el);
 })
 
@@ -15,14 +18,17 @@ arrColors.forEach((item, i) => {
     el.css('padding', '10px');
     $('#labels').append(el);
     el.on('click', function () {
-        const color = arrColors[i];          
-        $(this).addClass('shadow');      
-        $('#wrap > div').each(function() {
-            console.log($(this).css('background-color'), color)
-            if ($(this).css('background-color') === color) {
+        const color = arrColors[i];
+        $('#labels > div').each(function () {
+            if ($(this).hasClass('shadow')) $(this).removeClass('shadow');
+        });
+        $(this).addClass('shadow');
+        $('#wrap > div').each(function () {
+            console.log($(this).attr('data-color'), color)
+            if ($(this).attr('data-color') === color) {
                 $(this).addClass('shadow');
             } else {
-                // $(this).removeClass('shadow');
+                $(this).removeClass('shadow');
             }
         })
     })
