2d0b09a1be33e299735cff3433b8fd12785cbc17
romb52 <rom.bom1980@gmail.com>
2023-10-07 21:51:41 +0300

    +

--- DZ_canvas_Simple-Paint/js/script.js
+++ DZ_canvas_Simple-Paint/js/script.js
@@ -1,21 +1,21 @@
 const colorItems = document.querySelectorAll('.btn-color');
-let rotated = false;
-const figureWidth = 70;
+let rotated = false;                        //костыль для ромба
+const figureWidth = 70;        //кастомные размеры фигур
 const figureHeight = 70;
-let newFigureWidth = figureWidth;
+let newFigureWidth = figureWidth;            //изменякмы размеры фигур, по умолчанию кастомные
 let newFigureHeight = figureHeight;
 
 
 //разрисовываем палитру цветов
-const defaultColor = document.getElementById('selectedColor').style.backgroundColor = "#000";
-colorItems.forEach(element => {
-    element.style.backgroundColor = element.value;
+const defaultColor = document.getElementById('selectedColor').style.backgroundColor = "#000";               //при загрузке по умолчанию выбраный цвет - черный
+colorItems.forEach(element => {                                                           
+    element.style.backgroundColor = element.value;                 //каждую кнопку раскрашиваем в соответствии с ее value                            
     element.addEventListener('click', (e) => {
-        document.getElementById('selectedColor').style.backgroundColor = e.target.value;
+        document.getElementById('selectedColor').style.backgroundColor = e.target.value;                //при клике на кнопки палитры меняем выбранный цвет
     })
 });
 
-function getDC(id) {
+function getDC(id) {                                          //функция проверки canvas
     let fig = document.getElementById(id);
     if (fig == null) {
         alert("Figure element not found");
@@ -28,8 +28,9 @@ function getDC(id) {
         return false;
     }
 }
-
-function rectangle(color = '#fff') {
+ 
+/* блок выбора фигуры*/
+function rectangle(color = '#fff') {                 //рисуем прямоугольник
     let dc = getDC("rect");
     if (dc == false) return;
     let canvas = document.getElementById("rect");
@@ -41,7 +42,7 @@ function rectangle(color = '#fff') {
 }
 rectangle();
 
-function circle(color = '#A6A6A6') {
+function circle(color = '#A6A6A6') {                      //рисуем круг
     let dc = getDC("circle");
     if (dc == false) return;
     let canvas = document.getElementById("circle");
@@ -53,7 +54,7 @@ function circle(color = '#A6A6A6') {
 }
 circle();
 
-function romb(color = '#A6A6A6') {
+function romb(color = '#A6A6A6') {                        //рисуем ромб
     let dc = getDC("romb");
     if (dc == false) return;
     let canvas = document.getElementById("circle");
@@ -71,7 +72,7 @@ function romb(color = '#A6A6A6') {
 }
 romb();
 
-function triangle(color = '#A6A6A6') {
+function triangle(color = '#A6A6A6') {                //рисуем треугольник
     let dc = getDC("triangle");
     if (dc == false) return;
     let canvas = document.getElementById("triangle");
@@ -93,12 +94,9 @@ function triangle(color = '#A6A6A6') {
 }
 triangle();
 
-
+/*при клике на фигуру делаем ее checked и выделяем цветом обводки */
 const figureItems = document.getElementById('selectBlock').getElementsByTagName('input');
-// console.log(figureItems);
-// figureItems.forEach((item) => {
-//     console.log(item);
-// });
+
 for (let item of figureItems) {
     //console.log(item.name,item.checked);
     item.addEventListener("click", (e) => {
@@ -133,133 +131,65 @@ for (let item of figureItems) {
 };
 
 let canvasDrawWindow = document.getElementById('drawWindow');
-const canvasWidth = document.getElementsByClassName('draw-window')[0].offsetWidth;
+const canvasWidth = document.getElementsByClassName('draw-window')[0].offsetWidth; //вычисляем размеры блока под рисование
 const canvasHeight = document.getElementsByClassName('draw-window')[0].offsetHeight;
 // console.log(canvasWidth, canvasHeight);
-canvasDrawWindow.width = canvasWidth;
+canvasDrawWindow.width = canvasWidth; //задаем полученные размеры как ширину и высоту canvas
 canvasDrawWindow.height = canvasHeight;
 
-// if (getDC("drawWindow")==false)return;
-// canvasDrawWindow.addEventListener('click', (e) => {
-//     let startPointX = e.clientX - canvasDrawWindow.getBoundingClientRect().left;;
-//     let startPointY = e.clientY - canvasDrawWindow.getBoundingClientRect().top;
-//     console.log(e.clientX, e.clientY);
-//     console.log(canvasDrawWindow.width, canvasDrawWindow.height);
-//     for (let item of figureItems) {
-//         if (item.checked == true) {
-//             let dc = getDC("drawWindow");
-//             let color = document.getElementById('selectedColor').style.backgroundColor;
-//             if (dc == false) return;
-//             const atr = item.getAttribute("data-atr");
-//             console.log(item.getAttribute("data-atr"));
-//             if (atr === 'rectangle') {
-//                 dc.beginPath();
-//                 dc.fillStyle = color;
-//                 dc.rect(startPointX, startPointY, figureWidth, figureHeight);
-//                 dc.fill();
-//                 console.log(startPointX, startPointY, color);
-
-//             }
-//             if (atr === 'circle') {
-//                 dc.beginPath();
-//                 dc.fillStyle = color;
-//                 dc.arc(startPointX, startPointY, figureWidth / 2, 0, 2 * Math.PI);
-//                 dc.fill();
-//                 console.log(startPointX, startPointY, color);
-//             }
-//             if (atr === 'romb') {
-//                 let x1 = startPointX;
-//                 let y1 = startPointY;
-//                 let x2 = startPointX + figureWidth / 2;
-//                 let y2 = startPointY - figureWidth / 2;
-//                 let x3 = startPointX + figureWidth;
-//                 let y3 = startPointY;
-//                 let x4 = startPointX + figureWidth / 2;
-//                 let y4 = startPointY + figureWidth / 2;
-//                 dc.beginPath();
-//                 dc.moveTo(x1, y1);
-//                 dc.lineTo(x2, y2);
-//                 dc.lineTo(x3, y3);
-//                 dc.lineTo(x4, y4);
-//                 dc.closePath();
-//                 dc.fillStyle = color;
-//                 dc.fill();
-//             }
-//             if (atr === 'triangle') {
-//                 dc.fillStyle = color;
-//                 dc.lineWidth = 3;
-//                 let x1 = startPointX;
-//                 let y1 = startPointY;
-//                 let x2 = startPointX;
-//                 let y2 = startPointY - figureWidth;
-//                 let x3 = startPointX + figureWidth;
-//                 let y3 = startPointY;
-//                 dc.beginPath();
-//                 dc.moveTo(x1, y1);
-//                 dc.lineTo(x2, y2);
-//                 dc.lineTo(x3, y3);
-//                 dc.closePath();
-//                 dc.fill();
-//             }
-//         }
-//     };
-// });
-
+/*обработка нажатия мыши на холсте*/
 canvasDrawWindow.addEventListener('mousedown', (e) => {
-    let startPointX = e.clientX;
-    let startPointY = e.clientY;
-    console.log(e.clientX, e.clientY);
-    console.log(canvasDrawWindow.width, canvasDrawWindow.height);
+    let startPointX = e.clientX - canvasDrawWindow.getBoundingClientRect().left; //координаты клика на полотне
+    let startPointY = e.clientY - canvasDrawWindow.getBoundingClientRect().top;
+    console.log(startPointX, startPointY);
     let isDragging = true;
-    canvasDrawWindow.addEventListener('mousemove', (e) => {
-        if (isDragging) {
-            let deltaX = e.clientX - startPointX;
-            let deltaY = e.clientY - startPointY;
-            newFigureWidth = deltaX;
-            newFigureHeight = deltaY;
-            console.log(deltaX, deltaY);
-        }
-    });
-    canvasDrawWindow.addEventListener('mouseup', (e) => {
-        isDragging = false;
-        canvasDrawWindow.removeEventListener('mousemove', (e));
-        canvasDrawWindow.removeEventListener('mouseup', (e));
+    newFigureWidth = figureWidth;              //каждый раз задаем на старте начальные размеры фигур, на случай клика без движения мыши (на холс нарисуются фигурки стандартного размера)
+    newFigureHeight = figureHeight;
+    canvasDrawWindow.addEventListener('mousemove', handleMouseMove);         //обработка mousemove и mouseup (обзываем чтоб можно было эти события снять)
+    canvasDrawWindow.addEventListener('mouseup', handleMouseUp);
 
-        startPointX = startPointX - canvasDrawWindow.getBoundingClientRect().left;;
-        startPointY = startPointY - canvasDrawWindow.getBoundingClientRect().top;
-        // console.log(e.clientX, e.clientY);
-        // console.log(canvasDrawWindow.width, canvasDrawWindow.height);
+    function handleMouseUp(e) {                                               //отжал кнопку
+        isDragging = false;
+        canvasDrawWindow.removeEventListener('mousemove', handleMouseMove);       //снимаем сразу события 
+        canvasDrawWindow.removeEventListener('mouseup', handleMouseUp);
+
         for (let item of figureItems) {
             if (item.checked == true) {
                 let dc = getDC("drawWindow");
-                let color = document.getElementById('selectedColor').style.backgroundColor;
+                let color = document.getElementById('selectedColor').style.backgroundColor;            //задаем цвет фигуры
                 if (dc == false) return;
                 const atr = item.getAttribute("data-atr");
                 console.log(item.getAttribute("data-atr"));
-                if (atr === 'rectangle') {
+                if (atr === 'rectangle') {                                          //если прямоугольник
                     dc.beginPath();
                     dc.fillStyle = color;
                     dc.rect(startPointX, startPointY, newFigureWidth, newFigureHeight);
                     dc.fill();
-                    console.log(startPointX, startPointY, color);
-    
+                    console.log(startPointX, startPointY, newFigureWidth, newFigureHeight, color);
                 }
-                if (atr === 'circle') {
+                if (atr === 'circle') {                                              //если круг
                     dc.beginPath();
                     dc.fillStyle = color;
-                    dc.arc(startPointX, startPointY, newFigureWidth / 2, 0, 2 * Math.PI);
+
+                    // startPointX + newFigureWidth / 2, startPointY + newFigureHeight / 2 - координати центра круга (середина вектора)
+                    //(Math.sqrt(Math.pow(newFigureWidth, 2) + Math.pow(newFigureHeight, 2))) / 2 - теорема Пиффагора, вычисляем радиус - половина вектора по координатам
+                    if (newFigureWidth === figureWidth && newFigureHeight === figureHeight) {
+                        dc.arc(startPointX, startPointY, figureWidth / 2, 0, 2 * Math.PI);
+                    } else //eсли по клику
+                        dc.arc(startPointX + newFigureWidth / 2, startPointY + newFigureHeight / 2, (Math.sqrt(Math.pow(newFigureWidth, 2) + Math.pow(newFigureHeight, 2))) / 2, 0, 2 * Math.PI);
                     dc.fill();
-                    console.log(startPointX, startPointY, color);
+                    console.log(startPointX + newFigureWidth / 2, startPointY + newFigureHeight / 2, newFigureWidth / 2, color);
+
                 }
-                if (atr === 'romb') {
+                if (atr === 'romb') {                                             //если ромб
                     let x1 = startPointX;
-                    let y1 = startPointY;
-                    let x2 = startPointX + newFigureWidth ;
-                    let y2 = startPointY - newFigureWidth ;
-                    let x3 = startPointX + newFigureWidth*2;
-                    let y3 = startPointY;
-                    let x4 = startPointX + newFigureWidth ;
-                    let y4 = startPointY + newFigureWidth ;
+                    let y1 = startPointY + newFigureHeight / 2;
+                    let x2 = startPointX + newFigureWidth / 2;
+                    let y2 = startPointY;
+                    let x3 = startPointX + newFigureWidth;
+                    let y3 = startPointY + newFigureHeight / 2;
+                    let x4 = startPointX + newFigureWidth / 2;
+                    let y4 = startPointY + newFigureHeight;
                     dc.beginPath();
                     dc.moveTo(x1, y1);
                     dc.lineTo(x2, y2);
@@ -269,15 +199,15 @@ canvasDrawWindow.addEventListener('mousedown', (e) => {
                     dc.fillStyle = color;
                     dc.fill();
                 }
-                if (atr === 'triangle') {
+                if (atr === 'triangle') {                          //если треугольник
                     dc.fillStyle = color;
                     dc.lineWidth = 3;
                     let x1 = startPointX;
                     let y1 = startPointY;
-                    let x2 = startPointX;
-                    let y2 = startPointY - newFigureWidth;
-                    let x3 = startPointX + newFigureWidth;
-                    let y3 = startPointY;
+                    let x2 = startPointX + newFigureWidth;
+                    let y2 = startPointY + newFigureHeight;
+                    let x3 = startPointX ;
+                    let y3 = startPointY + newFigureHeight;
                     dc.beginPath();
                     dc.moveTo(x1, y1);
                     dc.lineTo(x2, y2);
@@ -286,12 +216,17 @@ canvasDrawWindow.addEventListener('mousedown', (e) => {
                     dc.fill();
                 }
             }
-        };
-    
-    });
+        }
+    }
 
-   
-
+    function handleMouseMove(e) {                                                              //обрабатываем движение курсора при надатии мыши
+        let deltaX = e.clientX - canvasDrawWindow.getBoundingClientRect().left - startPointX;     
+        let deltaY = e.clientY - canvasDrawWindow.getBoundingClientRect().top - startPointY;
+        newFigureWidth = deltaX;
+        newFigureHeight = deltaY;
+        // console.log(deltaX, deltaY);        
+    }
 });
+
 
 
