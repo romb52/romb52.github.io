d96c874dfae5a7fb6a5dd91902a01de3beb67e8b
romb52 <rom.bom1980@gmail.com>
2023-12-10 22:01:19 +0200

    +

--- hw_1_typescript/src/index.ts
+++ hw_1_typescript/src/index.ts
@@ -1,4 +1,27 @@
-const word:string = 'hello world';
-const arr:string[] = [];
+const word: string = 'hello world';
+const arr: string[] = [];
 
 console.log(word);
+
+function getWord(word: string): string {
+    return word;
+
+}
+console.log(getWord('1'));
+
+function noth(): void {
+    const a: number = 1;
+    const b: number = 2;
+    console.log(a + b);
+}
+
+noth();
+
+interface IBook {
+    title: string;
+    count: number;
+    isExist: boolean;
+}
+
+const obj:IBook = { title: 'Book', count: 1, isExist: true }
+const booksArray: IBook[]=[obj ];
