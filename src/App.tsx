f39067d6b25c1cc872db41ad78eee5f9d7f43a51
Lyubov Myetolkina <luba150278@gmail.com>
2023-12-12 20:59:18 +0200

    all

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React, { useState } from 'react';
-import './App.css';
-import ChildComponent from './components/ChildComponent/ChildComponent';
+import React from 'react';
+import { withLayout } from './components/Layout/Layout';
+
 
 function App(): JSX.Element {
-  const [count, setCount] = useState<number>(0);
-  const changeFunc = (a: number): void => {
-    if (a < 10) {
-      setCount(count + 1)
-    } else {
-      setCount(count - 1)
-    }
-  }
+ 
   return (
-    <div className='App'>
+    <div className='container'>
       <h1>Hello</h1>
-      <p>{ count}</p>
-      
-      <ChildComponent changeFunc={changeFunc} count={count} />
     </div>
   );
 }
 
-export default App;
+export default withLayout(App);
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import App from './App';
+import Auth from './components/Auth/Auth';
+import { Provider } from 'react-redux';
+import { store } from './share/store';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/auth",
+    element: <Auth />,
+  },
+]);
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}><RouterProvider router={router} /></Provider>
   </React.StrictMode>
 );
 
