c5f3f4d0ff8c1770bdbaacc92eaea8459086e896
romb52 <rom.bom1980@gmail.com>
2024-01-02 21:15:51 +0200

    +

--- practise-react/cw_6_react_app-login/src/components/app/App.jsx
+++ practise-react/cw_6_react_app-login/src/components/app/App.jsx
@@ -1,16 +1,37 @@
-
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Offcanvas from 'react-bootstrap/Offcanvas';
 
 function App() {
- 
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+
   return (
-    <>      
+    <>
       <section>
         <div className='container'>
-         <h1>Home page</h1>
+          <h1>Home page</h1>
+          <Button variant="primary" onClick={handleShow}>
+            Launch
+          </Button>
+
+          <Offcanvas show={show} onHide={handleClose}>
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              Some text as placeholder. In real life you can have the elements you
+              have chosen. Like, text, images, lists, etc.
+            </Offcanvas.Body>
+          </Offcanvas>
         </div>
-      </section>      
+      </section>
     </>
   );
 }
 
 export default App;
+
