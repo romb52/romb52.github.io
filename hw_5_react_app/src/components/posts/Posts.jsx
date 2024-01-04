830e09fd2c014c867d98f2fa00a258f47d863221
romb52 <rom.bom1980@gmail.com>
2024-01-04 12:58:09 +0200

    improve code, add css, add  display an error message

--- hw_5_react_app/src/components/posts/Posts.jsx
+++ hw_5_react_app/src/components/posts/Posts.jsx
@@ -6,14 +6,21 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { translate } from '../../share/translate';
 import { Link } from 'react-router-dom';
+import { FaHome } from 'react-icons/fa';
 
-export default function Posts({lang}) {
+export default function Posts({ lang }) {
   const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     async function getPosts() {
-      const res = await axios.get('https://dummyjson.com/posts');
-      setPosts(res.data.posts);
+      try {
+        const res = await axios.get('https://dummyjson.com/posts');
+        setPosts(res.data.posts);
+      } catch (error) {
+        console.error('Error fetching posts:', error);
+        setError('Error fetching posts. Please try again later.');
+      }
     }
     getPosts();
   }, []);
@@ -22,29 +29,33 @@ export default function Posts({lang}) {
     <section >
       <div className='container'>
         <h1>Posts</h1>
-        <div className={styles.posts}>
-          {posts.map((item) => (
-            <Card key={item.id}>
-              <Card.Body className='d-flex flex-column justify-content-between'>
-                <Card.Title>{item.title}</Card.Title>
-                <Card.Text>
-                  {item.body.slice(0, 40)}...
-                </Card.Text>
-                <Card.Text>
-                  {item.tags.map((tag, index) => (
-                    <span className={styles.tags} key={index}>
-                      #{tag}
-                      {index !== item.tags.length - 1 && " "}
-                    </span>
-                  ))}
-                </Card.Text>
-                <Card.Text className={styles.likes}><AiFillLike size={20} />{item.reactions}</Card.Text>
-                <Link to={`/post/${item.id}`}><Button  variant="primary">{translate[lang]['more']}</Button></Link>
-                
-              </Card.Body>
-            </Card>
-          ))}
-        </div>
+        {error ? (
+          <div className={styles.error}>{error}<Link className={styles.linkHome} to='/'><FaHome className={styles.homeIcon} /></Link></div>
+        ) : (
+          <div className={styles.posts}>
+            {posts.map((item) => (
+              <Card key={item.id}>
+                <Card.Body className='d-flex flex-column justify-content-between'>
+                  <Card.Title>{item.title}</Card.Title>
+                  <Card.Text>
+                    {item.body.slice(0, 40)}...
+                  </Card.Text>
+                  <Card.Text>
+                    {item.tags.map((tag, index) => (
+                      <span className={styles.tags} key={index}>
+                        #{tag}
+                        {index !== item.tags.length - 1 && " "}
+                      </span>
+                    ))}
+                  </Card.Text>
+                  <Card.Text className={styles.likes}><AiFillLike size={20} />{item.reactions}</Card.Text>
+                  <Link to={`/post/${item.id}`}><Button variant="primary">{translate[lang]['more']}</Button></Link>
+
+                </Card.Body>
+              </Card>
+            ))}
+          </div>
+        )}
       </div>
     </section>
   );
