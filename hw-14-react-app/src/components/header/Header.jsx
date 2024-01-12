cc13f59f4e0e7732b383fbd4d7b4ff8286eaafb2
romb52 <rom.bom1980@gmail.com>
2024-01-12 22:57:59 +0200

    +

--- hw-14-react-app/src/components/header/Header.jsx
+++ hw-14-react-app/src/components/header/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
     const handleThemeChange = (theme) => {
         if (theme === 'random') {
             dispatch(random());
-        }
+        } else
         if (theme === 'auto') {
             dispatch(setAutoTheme());
         } 
